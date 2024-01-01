d94f21e39b695cd891793c325261a630d46b4260
Diyi Wang <wang.di@northeastern.edu>
2024-01-01 15:59:33 +0800

    Implemented time machine

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,45 +1,72 @@
-import React, { useState, useEffect } from 'react';
-import SudokuGrid from './SudokuGrid';
-import { generateSudoku } from './Utils';
-import { useParams } from 'react-router-dom';
-
+import React, { useState, useEffect } from "react";
+import SudokuGrid from "./SudokuGrid";
+import { generateSudoku } from "./Utils";
+import { useParams } from "react-router-dom";
 
 function Game() {
-  const [sudoku, setSudoku] = useState(generateSudoku());
+  const [staticSudoku, setStaticSudoku] = useState([]);
+  const [history, setHistory] = useState(() => {
+    let history = [];
+    let s = generateSudoku();
+    history.push(s);
+    setStaticSudoku(JSON.parse(JSON.stringify(s)));
+    return history;
+  });
+  const [current, setCurrent] = useState(0);
   const [isValid, setIsValid] = useState(false);
-  const [staticSudoku, setStaticSudoku] = useState(sudoku);
+
   const params = useParams();
   const id = params.id;
 
-  // 处理用户输入
+  // Change Handling
   const handleCellChange = (value, row, col) => {
-    const newSudoku = [...sudoku];
+    let sudoku = history[current];
+    const newSudoku = JSON.parse(JSON.stringify(sudoku));
     newSudoku[row][col] = parseInt(value);
-    setSudoku(newSudoku);
+    let newHistory = history.slice(0, current + 1);
+    newHistory.push(newSudoku);
+    setHistory(newHistory);
   };
 
-  // 当用户输入改变时更新状态
+  // Hooks
   useEffect(() => {
-    newStatic(sudoku);
-  }, []);
+    setCurrent(history.length - 1);
+  }, [history]);
 
-  const newStatic = (newSudoku) => {
-    setStaticSudoku(JSON.parse(JSON.stringify(newSudoku)));
-  };
+  useEffect(() => {
+    console.log("current pointer is changed!")
+    console.log(current);
+    console.log(history[current]);
+  }, [current]);
 
+  // Button Reactions
   const restartGame = () => {
     const newSudoku = generateSudoku();
-    setSudoku(newSudoku);
-    setIsValid(false);
-    newStatic(newSudoku);
+    setStaticSudoku(JSON.parse(JSON.stringify(newSudoku)));
+    setHistory([newSudoku]);
+  };
+
+  const backwards = () => {
+    setCurrent(current > 0 ? current - 1 : current);
+  };
+
+  const forwards = () => {
+    setCurrent(current < history.length - 1 ? current + 1 : current);
   };
 
   return (
     <div className="App">
       <h1>Sudoku</h1>
       <h3>Session Id: {id}</h3>
+      <h3>Steps recorded: {history.length}</h3>
       <button onClick={restartGame}>Restart</button>
-      <SudokuGrid sudoku={sudoku} onChange={handleCellChange} isDisabled={(row, col) => staticSudoku[row][col] !== 0}/>
+      <button onClick={backwards}>Step Back</button>
+      <button onClick={forwards}>Continue</button>
+      <SudokuGrid
+        sudoku={current >= history.length ? history[history.length - 1] : history[current]}
+        onChange={handleCellChange}
+        isDisabled={(row, col) => staticSudoku[row][col] !== 0}
+      />
     </div>
   );
 }
